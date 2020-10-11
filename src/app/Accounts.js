01da879f17a32e2e11515384dbae7d1875c87f2c
samuel <shgoldfarbh@gmail.com>
2020-10-11 19:05:55 -0300

    feat: add material ui

--- src/app/Accounts.js
+++ src/app/Accounts.js
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
+import { Delete } from "@material-ui/icons";
+import { IconButton } from "@material-ui/core";
 
 const Accounts = () => {
   const [accounts, setAccounts] = useState([
     { id: 1, name: "Cash" },
     { id: 2, name: "Bank" },
   ]);
-  return <div>{JSON.stringify(accounts)}</div>;
+  return (
+    <div>
+      {accounts.map((account) => (
+        <div key={account.id}>
+          {account.name}
+          <IconButton
+            aria-label="delete"
+            onClick={() =>
+              setAccounts(accounts.filter(({ id }) => id !== account.id))
+            }
+          >
+            <Delete />
+          </IconButton>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Accounts;
